371f740a1fb206c6b74e757346a1797020f09794
David Magnus Henriques <david.magnus@acagamics.de>
2017-01-11 01:20:40 +0100

    Graphs and Javascript are now nicer

--- CivBudget/settings.py
+++ CivBudget/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'graphos',
 ]
 
 MIDDLEWARE = [
@@ -115,5 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 LOGIN_URL = '/login'
--- budget/views.py
+++ budget/views.py
@@ -6,8 +6,17 @@ from dateutil import parser
 from .models import *
 from .utils import Utils
 from django.utils import timezone
+import datetime
 from datetime import timedelta
 from django.db.models import Sum
+from graphos.sources.model import SimpleDataSource
+from graphos.renderers.morris import BarChart
+from collections import Counter
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
 
 @login_required
@@ -18,19 +27,31 @@ def index(request):
     first_day_of_month = today.replace(day=1)
     monday_of_this_week = today - timedelta(days=today.weekday())
     rate_list = Rate.objects.filter(user=user).order_by('daily_value')
-    booking_list = Booking.objects.filter(user=user).order_by('value').filter(date__range=[today, now])
+    bookings_of_day = Booking.objects.filter(user=user).order_by('value').filter(date__range=[today, now])
     bookings_of_week = Booking.objects.filter(user=user).order_by('value').filter(
         date__range=[monday_of_this_week, now]).aggregate(balance_of_week=Sum('value'))
     bookings_of_month = Booking.objects.filter(user=user).order_by('value').filter(
         date__range=[first_day_of_month, now]).aggregate(balance_of_month=Sum('value'))
+    bookings = Booking.objects.filter(user=user).order_by('date')
+    start_date = bookings[0].date
+    end_date = today
+    aggregated_by_day = [['Date', 'Value']]
+    for single_date in daterange(start_date, end_date):
+        bookings_up_to_day = Booking.objects.filter(user=user).filter(
+            date__range=[start_date, single_date]).aggregate(
+            balance_up_to_day=Sum('value'))['balance_up_to_day']
+        aggregated_by_day.append([single_date, bookings_up_to_day])
+    data_source = SimpleDataSource(aggregated_by_day)
+    chart = BarChart(data_source)
     context = {
-        'booking_list': booking_list,
+        'bookings_of_day': bookings_of_day,
         'rate_list': rate_list,
         'magic_number': Utils.calculate_daily_rate(rate_list),
-        'booking_total': Utils.calculate_total(booking_list),
+        'booking_total': Utils.calculate_total(bookings_of_day),
         'today': timezone.now(),
         'balance_of_week': bookings_of_week['balance_of_week'],
         'balance_of_month': bookings_of_month['balance_of_month'],
+        'chart': chart,
 
     }
     return render(request, 'budget/index.html', context)
